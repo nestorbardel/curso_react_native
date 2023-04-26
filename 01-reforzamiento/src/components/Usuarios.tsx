17513ccefb7cb7e95d63d01ae8d1949a2f040fa4
Nestor <nestorbardel@gmail.com>
2023-04-26 15:08:54 -0300

    deleted node_modules

--- 01-reforzamiento/src/components/Usuarios.tsx
+++ 01-reforzamiento/src/components/Usuarios.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react"
+import { reqResApi } from "../api/reqRes"
+
 export const Usuarios = () => {
+
+  useEffect(() => {
+    reqResApi.get('/users')
+    .then(resp => console.log(resp.data.data))
+    .catch(console.log)
+  }, [])
+  
+
   return (
     <>
         <h3>Usuarios: </h3>
