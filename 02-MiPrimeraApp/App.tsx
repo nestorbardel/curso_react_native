ca69c9b64f07ecaf3be26bf7dc77ea0338c5ab99
Nestor <nestorbardel@gmail.com>
2023-05-10 07:29:04 -0300

    Primer App

---
+++ 02-MiPrimeraApp/App.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
+
+export const App = () => {
+  return (
+    <HolaMundoScreen/>
+  );
+};
---
+++ 02-MiPrimeraApp/src/screens/HolaMundoScreen.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react'
+import { Text, View } from 'react-native';
+
+export const HolaMundoScreen = () => {
+  return (
+    <View style={{
+        flex: 1,
+        // backgroundColor: 'red',
+        justifyContent: 'center',
+      }}
+      >
+        <Text style={{
+          fontSize: 45,
+          textAlign: 'center'
+        }}
+        >Hola Mundo</Text>
+      </View>
+  )
+}
